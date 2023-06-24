6f7488cd0947d9a1210127db4f5024acb4910d14
fGibault <117034325+fGibault@users.noreply.github.com>
2023-06-24 12:23:57 -0400

    Add files via upload
    3 models de tables mongoose(Adresse doit changer de format)
    create, update, details et delete sur table hebergement dans HebergementController
    de simples routes pour chacune dans hebergementRoutes

---
+++ models/Hebergement.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const HebergementSchema = new mongoose.Schema({
+  idHebergement: mongoose.Schema.Types.ObjectId,
+  titre: String,
+  description: String,
+  imageSrc: String,
+  categorie: {
+    type: String,
+    enum : ['Chalet','Appartement','Maison'],
+  },
+  nbChambres: Number,
+  nbSalleBains: Number,
+  nbPersonneMax: Number,
+  localisation: String,
+  prix: Number,
+  user: mongoose.Schema.Types.ObjectId,
+  reservations: [mongoose.Schema.Types.ObjectId],
+  animalAccepte: Boolean
+});
+
+module.exports = mongoose.model('Hebergement', HebergementSchema);
